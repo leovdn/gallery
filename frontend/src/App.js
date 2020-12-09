a115e17eca3da738b4747c0ccd219d0a18fc0c2c
leovdn <leo_vdn@hotmail.com>
2020-12-08 23:36:30 -0300

    test repo correction

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Gallery Project</h1>
+      <p>A project for React State practice</p>
+    </>
+  );
+}
+
+export default App;
